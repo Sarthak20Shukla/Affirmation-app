2e562ff67360f23d37734293ee60f538b346d3fd
Sarthak Shukla <sarthakshukla500@gmail.com>
2022-10-12 16:14:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import java.time.ZoneId
+
+data class Affirmation(val stringResourceId: Int,@DrawableRes val imageResourceId:Int)
